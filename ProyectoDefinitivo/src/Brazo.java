d051102ef1ceea611a0ed59a8259c44b3b2ef0e4
JavierRosalesRojas <javier.thespant@gmail.com>
2018-05-19 13:47:59 -0400

    ProyectoPauta

---
+++ ProyectoDefinitivo/src/Brazo.java
@@ -0,0 +1,12 @@
+public class Brazo {
+
+    private  String lado_brazo;
+
+    public String getLado_brazo() {
+        return lado_brazo;
+    }
+
+    public void setLado_brazo(String lado_brazo) {
+        this.lado_brazo = lado_brazo;
+    }
+}
